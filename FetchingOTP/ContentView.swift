f89115e48c0f965f6917444f3e8c0f17066b196b
lenny_cheng <langrisser1981@gmail.com>
2022-12-09 09:20:13 +0800

    Initial Commit

---
+++ FetchingOTP/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  FetchingOTP
+//
+//  Created by 程信傑 on 2022/12/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FetchingOTP/FetchingOTPApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchingOTPApp.swift
+//  FetchingOTP
+//
+//  Created by 程信傑 on 2022/12/9.
+//
+
+import SwiftUI
+
+@main
+struct FetchingOTPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
